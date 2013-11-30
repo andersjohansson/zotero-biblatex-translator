de944ff506605c0d16c15c2fc4aa21c727d682ba
Anders Johansson <mejlaandersj@gmail.com>
2013-11-30 18:14:09 +0100

    Clean up writing of titles and publishers. Use "journaltitle" instead of "journal". Use "institution" instead of "school".

--- BibLaTeX-with-hacks.js
+++ BibLaTeX-with-hacks.js
@@ -15,7 +15,7 @@
 		"exportFileData": false,
 		"useJournalAbbreviation": false
     },
-    "lastUpdated":"2013-10-28 09:03"
+    "lastUpdated":"2013-11-30 18:13"
 }
 
 //%a = first author surname
@@ -400,52 +400,30 @@ function doExport() {
             }
         }
 
-
+        //publicationTitles and special titles
 	if(item.publicationTitle) {
-	    if(item.itemType == "bookSection" || item.itemType == "conferencePaper") {
+	    if(item.itemType == "bookSection" || item.itemType == "conferencePaper" || item.itemType == "dictionaryEntry" || item.itemType == "encyclopediaArticle") {
 		writeField("booktitle", item.publicationTitle); 
 	    } else if (item.itemType == "magazineArticle" || item.itemType == "newspaperArticle"){
                 writeField("journaltitle", item.publicationTitle);
 	    } else if (item.itemType == "journalArticle") {
                 if(Zotero.getOption("useJournalAbbreviation")){
-		    writeField("journal", item.journalAbbreviation);
+		    writeField("journaltitle", item.journalAbbreviation);
                 } else {
                     writeField("journaltitle", item.publicationTitle);
                     writeField("shortjournal", item.journalAbbreviation);
                 }
             }
-
-	   // else if (item.itemType == "website" || item.itemType == "forumPost" || item.itemType == "blogPost" || item.itemType == "tvBroadcast" || item.itemType == "radioBroadcast") {
-	//	writeField("titleaddon", item.publicationTitle);
-	// 
-		//do nothing as websiteTitle, forumTitle, blogTitle,
-		//programTitle seems
-		//to be just aliases for publicationTitle and already
-		//are correctly mapped below
-//	    }
-	    else { 
-		//writeField("journaltitle", item.publicationTitle);
-		//TODO, did we miss something
-	    }
 	}
 	
-
-    //TODO: check what happens to bookTitle, is that also an alias for publicationTitle?
-
-	
-	if(item.encyclopediaTitle || item.dictionaryTitle || item.proceedingsTitle) {
-	    writeField("booktitle",item.encyclopediaTitle || item.dictionaryTitle || item.proceedingsTitle);
-	}
-
 	if(item.websiteTitle || item.forumTitle || item.blogTitle || item.programTitle) {
 	    writeField("titleaddon", item.websiteTitle || item.forumTitle || item.blogTitle || item.programTitle);
 	}
 	
 	
+        //publishers
 	if(item.publisher) {
-	    if(item.itemType == "thesis") {
-		writeField("school", item.publisher); //school is an acceptable alias in biblatex
-	    } else if(item.itemType =="report") {
+	    if(item.itemType == "thesis" || item.itemType =="report") {
 		writeField("institution", item.publisher);
 	    } else {
 		writeField("publisher", item.publisher);
